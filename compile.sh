7e39a66d55726094139accbb79dfb1d6446df135
thiago <thiago.haine@hotmail.com>
2017-10-16 05:33:59 -0200

    Muitos erros corrigidos!

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 clear;
 echo "Qual o nome do arquivo a compilar?(ex:file.c)"
 read user_input;
-gcc -o ${user_input%??} ${user_input} -lSDL -lSDL_image
+gcc -o ${user_input%??} ${user_input} -lSDL -lSDL_image -lSDL_ttf -lSDL_mixer
 #./game
--- game.c
+++ game.c
@@ -2,45 +2,58 @@
 #include<stdlib.h>
 #include"SDG.h"
 
+void room1();
+
 int main(){
 initSDG();
-scene *room = initScene(640,480,640,480);
-font *txt=newFont("font.ttf",48);
-sprite *spr_block=new_sprite("sprites/block.png",1);
-object *block=new_object("player",spr_block);
-//block->gravity=0.1;
-sceneElement *iBlock=instantiate(block,room,300,300);
-camera *main_camera = newCamera(640,480,iBlock);
-while(1){
-  while(checkEvent(room)){
-    if (sceneEvent(room).type==e_quit){
-      return 1;
-    }else if (sceneEvent(room).type==e_keydown){
-      switch (sceneEvent(room).keyCheck) {
-        case k_esc:
-          return 1;
-          break;
-        case k_up:
-          block->vspeed=-2;
-          break;
-        case k_left:
-          block->hspeed=-2;
-          block->vspeed=0;
-          break;
-        case k_right:
-          block->hspeed=2;
-          block->vspeed=0;
-          break;
-      }
-    }else if (sceneEvent(room).type==e_keyup){
-      if(sceneEvent(room).keyCheck==k_left || sceneEvent(room).keyCheck==k_right){
-        block->hspeed=0;
-      }
-    }
-    }
-    drawText("Testando a escrita de textos",txt,room);
-  drawScene(room,main_camera,"jogo de testes");
-}
+room1();
 quitSDG();
 return 1;
+}
+
+void room1(){
+  scene *room=initScene(640,480,640,480);
+  camera *cmr=newCamera(640,480,NULL);
+  int menu=0,go=1;
+  font *fnt_logo=newFont("font.ttf",48);
+  font *fnt_menu=newFont("font.ttf",16);
+  sprite *spr_logo=newText("Exemplo",fnt_logo);
+  sprite *spr_menu=newText("-----Menu-----",fnt_menu);
+  addTextSequence(spr_menu,"Start",fnt_menu,1);
+  addTextSequence(spr_menu,"Config",fnt_menu,1);
+  addTextSequence(spr_menu,"Exit",fnt_menu,1);
+  addTextSequence(spr_menu,">",fnt_menu,1);
+  object *obj_logo=new_object("Logo",spr_logo);
+  object *obj_menu=new_object("Menu",spr_menu);
+  instantiate(obj_logo,room,200,100);
+  instantiateImage(obj_menu,room,250,300,1);
+  instantiateImage(obj_menu,room,250,320,2);
+  instantiateImage(obj_menu,room,250,340,3);
+  instantiateImage(obj_menu,room,250,360,4);
+  sceneElement *selector=instantiateImage(obj_menu,room,230,320+(20*menu),5);
+  while(go==1){
+    while(checkEvent(room)){
+      if (sceneEvent(room).type==e_keyup){
+        if (sceneEvent(room).keyCheck==k_down){
+          if (menu<2){
+            menu++;
+          }else{
+            menu=0;
+          }
+        }
+      }else if (sceneEvent(room).keyCheck==k_up){
+        if (menu>0){
+          menu--;
+        }else{
+          menu=2;
+        }
+      }else if (sceneEvent(room).keyCheck==k_enter){
+        if (menu==2){
+        go=0;
+        }
+      }
+    }
+    selector->y=320+(20*menu);
+  drawScene(room,cmr,"Room1");
+  }
 }
