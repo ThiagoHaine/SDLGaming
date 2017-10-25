8a9f1e8179c51a7055e978f7b6cd647af71fe1b7
thiago <thiago.haine@hotmail.com>
2017-10-25 03:22:54 -0200

    SDG 2.0 disponível

--- instaladores/instalador windows/win-installer.bat
+++
@@ -1,6 +0,0 @@
-@echo off  
-pushd "%~dp0"
-move %cd%\windows\* C:\Windows\
-move %cd%\mingw-files\* C:\MinGW\
-setx Path "%Path%;C:\MinGW\bin" /m
-pause
--- instaladores/instalador_ubuntu.sh
+++
@@ -1,1 +0,0 @@
-sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
